f8b9616ccc598c805ef7883f397cfc686b246387
ShabanaRoshanAli <shabanaroshanali331@gmail.com>
2023-09-24 06:30:26 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const ActivityRoute = require('./routes/activityRoute')
+const UserRoute= require('./routes/userRoutes')
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv')
+const database = require('./config/database')
+const bodyParser = require('body-parser')
+
+app.use(express.json())
+
+app.use(cors())
+app.use(bodyParser.json());
+database()
+
+const port = process.env.PORT || 8000
+
+app.get('/', async (req, res) => {
+    res.send('Hello Api')
+})
+
+app.use('/api/activity', ActivityRoute)
+app.use('/api/users', UserRoute)
+
+app.listen(port, () => {
+    console.log(`Listening port on ${port}`);
+})
