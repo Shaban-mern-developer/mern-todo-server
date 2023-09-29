d9ed2f302b0cc6d745de382eca918b5c904f506a
ShabanaRoshanAli <shabanaroshanali331@gmail.com>
2023-09-29 12:55:33 -0700

    changes

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-
-const ActivityRoute = require('./routes/activityRoute')
-const UserRoute= require('./routes/userRoutes')
+const ActivityRoute = require('./routes/activityRoute');
+const UserRoute = require('./routes/userRoutes');
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv')
-const database = require('./config/database')
-const bodyParser = require('body-parser')
+const dotenv = require('dotenv');
+const database = require('./config/database');
+const bodyParser = require('body-parser');
 
 app.use(express.json());
-
 app.use(cors());
 app.use(bodyParser.json());
 
-database()
+// Initialize database connection
+database();
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.get('/', async (req, res) => {
-    res.send('Hello Api')
-})
+  res.send('Hello API');
+});
+
+app.use('/api/activity', ActivityRoute);
+app.use('/api/users', UserRoute);
+
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-  });
-  
-app.use('/api/activity', ActivityRoute)
-app.use('/api/users', UserRoute)
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
 
 app.listen(port, () => {
-    console.log(`Listening port on ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
